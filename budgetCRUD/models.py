19bbacca64ba4286f9fe582da4db4acd3766e814
justiniantheemperor <justinrellis52@gmail.com>
2021-11-20 23:33:33 -0700

    add blank database info

--- budgetCRUD/models.py
+++ budgetCRUD/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Spending_Category(models.Model):
+    category_id = models.CharField(max_length=2, unique=True)
+    description = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'grade_level'
+
+    def __str__(self) :
+        return (self.description)
+
