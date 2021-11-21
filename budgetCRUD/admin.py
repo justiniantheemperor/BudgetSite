0526619bdef2cb4844e2e4ab75dae61e56ee4525
Brandon <brandonjmhunt@gmail.com>
2021-11-20 23:57:23 -0700

    Admin model register

--- budgetCRUD/admin.py
+++ budgetCRUD/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import user,transaction,budget
 
 # Register your models here.
+admin.site.register(user)
+admin.site.register(transaction)
+admin.site.register(budget)
